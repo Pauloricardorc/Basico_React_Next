c4643522722362b56a3350815c129c0c3a9b453a
paulorrc <pauloricardorc0@gmail.com>
2021-08-24 01:05:29 -0400

    Criando pagina de info dos repositorio e fazendo paginacao
    foi criado uma pagina pra listar as informações sobre as issues do repositorio e tambem foi feito a paginacao

--- repos/src/pages/Main/index.js
+++ repos/src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback, useEffect } from 'react'
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa'
 import { Container, Form, SubmitButton, List, DeleteButton } from './style'
 import api from '../../services/api'
+import { Link } from 'react-router-dom';
 
 export default function Main() {
 
@@ -106,9 +107,9 @@ export default function Main() {
                             </DeleteButton>
                             {repo.name}
                         </span>
-                        <a href="">
+                        <Link to={`/repositorio/${encodeURIComponent(repo.name)}`}>
                             <FaBars size={20} />
-                        </a>
+                        </Link>
                     </li>
                 ))} 
             </List>
--- repos/src/pages/Repositorio/index.js
+++ repos/src/pages/Repositorio/index.js
@@ -1,9 +1,115 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './styles'
+import { BackButton, Container, IssuesList, Loading, Owner, PageActions } from './styles'
+import { FaArrowLeft } from 'react-icons/fa'
+import api from '../../services/api'
 
-export default function Repositorio() {
-    return(
-        <div>
-            <h1>Repostorios</h1>
-        </div>
+export default function Repositorio({ match }) {
+
+    const [repositorio, setRepositorio] = useState({})
+    const [issues, setIssues] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [page, setPage] = useState(1)
+
+    useEffect(() => {
+        async function load() {
+            const nameRepo = decodeURIComponent(match.params.repositorio)
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nameRepo}`),
+                api.get(`/repos/${nameRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            setRepositorio(repositorioData.data)
+            setIssues(issuesData.data)
+            setLoading(false)
+        }
+
+        load()
+    }, [match.params.repositorio])
+
+    useEffect(() => {
+
+        async function loadIssues() {
+            const nomeRepo = decodeURIComponent(match.params.repositorio)
+
+            const response = await api.get(`/repos/${nomeRepo}/issues`, {
+                params: {
+                    state: 'open',
+                    page,
+                    per_page: 5
+                }
+            });
+
+            setIssues(response.data)
+        }
+
+        loadIssues()
+
+    }, [match.params.repositorio, page])
+
+    function handlePage(action) {
+        setPage(action === 'back' ? page - 1 : page + 1)
+    }
+
+    if (loading) {
+        return (
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        )
+    }
+
+    return (
+        <Container>
+            <BackButton to="/">
+                <FaArrowLeft color="#333" size={30} />
+            </BackButton>
+            <Owner>
+                <img
+                    src={repositorio.owner.avatar_url}
+                    alt={repositorio.owner.login}
+                />
+                <h1>{repositorio.name}</h1>
+                <p>{repositorio.description}</p>
+            </Owner>
+
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a>
+                                {issue.labels.map(label => (
+                                    <span key={String(label.id)}>{label.name}</span>
+                                ))}
+                            </strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
+
+            <PageActions>
+                <button 
+                    type="button" 
+                    onClick={() => 
+                    handlePage('back')}
+                    disabled={page < 2}
+                >
+                    Voltar
+                </button>
+                <button type="button" onClick={() => handlePage('next')}>
+                    Proximo
+                </button>
+            </PageActions>
+        </Container>
     )
 }
